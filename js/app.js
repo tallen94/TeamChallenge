32f3fb241f062c09583f154ca01af222871eb691
Trevor Allen <tallen94@uw.edu>
2015-11-15 18:49:49 -0800

    adds all tests

--- js/app.js
+++ js/app.js
@@ -3,5 +3,27 @@
 angular.module("app", [])
 
 .controller("MainCtrl", ['$scope', function($scope) {
-	
+
 }])
+
+.directive('sameAs', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elem, attrs, ngModel) {
+            ngModel.$parsers.unshift(validate);
+
+            // Force-trigger the parsing pipeline.
+            scope.$watch(attrs.sameAs, function() {
+                ngModel.$setViewValue(ngModel.$viewValue);
+            });
+
+            function validate(value) {
+                var isValid = scope.$eval(attrs.sameAs) == value;
+
+                ngModel.$setValidity('same-as', isValid);
+
+                return isValid ? value : undefined;
+            }
+        }
+    };
+});
